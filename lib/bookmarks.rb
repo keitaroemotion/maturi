6a1fd1087c2a21612973bd229a47394232a10fd1
keisugano <tobasojyo@gmail.com>
2016-10-19 12:01:53 +0900

    bookmark can alter keyword in the middle of the searching

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -6,27 +6,50 @@ class Bookmarks
     bookmark_dir = "/Users/keisugano/Library/Application Support/Firefox/Profiles/u0tbyzbw.default"
     dbpath = "#{bookmark_dir}/places.sqlite"
 		db = SQLite3::Database.new(dbpath)
-		print "keyword: "
-		keyword = $stdin.gets.chomp
 
     records = db.execute("select distinct p.url, p.title from moz_places p join moz_bookmarks b where p.title=b.title group by b.title ")
 		db.close
 
-		records.each do |results|
-			if Inspector.matchesKeyword(results, keyword)
-				print "#{results[1].green} [open: y] "
-				if($stdin.gets.chomp == 'y')
-					%x( open "#{results[0]}" )
-					break
-				end
+    execute records, getKeyword
+
+  end
+ 
+  def self.getKeyword()
+    print "keyword: "
+		return $stdin.gets.chomp
+  end
+ 
+  def self.matchesKeyword(record, keyword)
+    begin
+      return (record[0]+record[1]).include?(keyword)
+    rescue
+      return false
+    end
+  end
+
+  def self.getLinkData(record)
+		print "#{record[1].green} [open: y] [search: s]"
+	  return $stdin.gets.chomp
+  end
+
+  def self.userRequestedOpeningLink(record)
+		return getLinkData(record) 
+  end
+
+  def self.execute(records, keyword)
+    records.each do |record|
+			if (matchesKeyword(record, keyword)) 
+        case userRequestedOpeningLink(record)
+        when 'y'
+				  	%x( open "#{record[0]}" )
+				  	break
+        when 's'
+            execute(records, getKeyword())
+            break 
+        end
 			end
 		end
   end
-  
 
-	
-  def execute()
-
-  end
 end
 
